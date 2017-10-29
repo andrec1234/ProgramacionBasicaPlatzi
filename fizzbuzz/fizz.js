aa154f52aabdd50d5d3df50b299eff3b5de6f070
Andric Vargas <andric@outlook.com>
2017-10-29 18:44:53 -0500

    Fizzbuzz con elseIF

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -5,15 +5,15 @@ var numeros = 100;
 
 for (var i=1; i<=numeros; i++){
 
-  if(i % 3 ==0 && i % 5 ==0)
+  if(esDivisible(i,3) && esDivisible(i,5))
   {
     document.write(i + " fizz buzz");
   }
-  else if( i % 3 == 0 )
+  else if( esDivisible(i,3) )
   {
       document.write(i + " fizz");
   }
-  else if( i % 5 == 0 )
+  else if( esDivisible(i,5))
   {
       document.write(i + " buzz");
   }
@@ -22,4 +22,16 @@ for (var i=1; i<=numeros; i++){
       document.write(i);
   }
   document.write("<br />");
+}
+
+function esDivisible(num, divisor)
+{
+  if(num % divisor == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
