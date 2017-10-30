8ac4b7d5a7e607fabd2b405d7bf7601a0490df2c
Andric Vargas <andric@outlook.com>
2017-10-29 19:46:22 -0500

    Proyecto Clases con Arrays

---
+++ Pakiman/paki.js
@@ -0,0 +1,12 @@
+var imagenes = [];
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Pokacho"] = "pollo.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
+var cauchin = new Pakiman("Cauchin",100,30);
+var pokacho = new Pakiman("Pokacho",80,50);
+var tocinauro = new Pakiman("Tocinauro",120,40);
+
+pokacho.mostrar();
+cauchin.mostrar();
+tocinauro.mostrar();
---
+++ Pakiman/pakiman.js
@@ -0,0 +1,26 @@
+class Pakiman
+{
+  constructor(name, life, attack)
+  {
+    this.imagen = new Image();
+    this.nombre = name;
+    this.vida = life;
+    this.ataque = attack;
+    //this.tipo = "tierra"; //
+
+    this.imagen.src = imagenes[this.nombre];
+  }
+  hablar ()
+  {
+    alert(this.nombre);
+  }
+  mostrar()
+  {
+    document.body.appendChild(this.imagen)
+    document.write("<p>");
+    document.write("<strong>" + this.nombre+ "</strong><br/>");
+    document.write("Vida: " + this.vida+ "<br/>");
+    document.write("Ataque: " + this.ataque+ "<br/><hr/>");
+    document.write("</p>");
+  }
+}
