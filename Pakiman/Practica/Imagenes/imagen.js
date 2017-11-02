a0c484c783d2532a9cfd6bad03fc2f46ba4cc102
Andric Vargas <andric@outlook.com>
2017-11-01 22:17:03 -0500

    Prueba de Imagenes con Diccionario de datos

--- Pakiman/Practica/Imagenes/imagen.js
+++ Pakiman/Practica/Imagenes/imagen.js
@@ -1,9 +1,20 @@
+//creando arreglo - diccionario de datos
+Imagenes = [];
+Imagenes['Pollo'] = "pollo.png";
+Imagenes['Cerdo'] = "cerdo.png";
+
 class imagen {
-  constructor(nombreImagen) {
+  constructor(nombreImagen,nombre,vida) {
     this.imagen = new Image();
     this.nombre = nombreImagen;
-    this.imagen.src = this.nombre;
+    this.animal = nombre,
+    this.vida = vida,
+    this.imagen.src = Imagenes[this.nombre];
+    document.write("<hr/>")
+    document.write("El es un: " + this.animal + "<br />");
+    document.write("Tiene de vida: " + this.vida + "<br />");
     document.body.appendChild(this.imagen);
   }
 }
-foto = new imagen("pollo.png");
+foto = new imagen("Pollo", "Pollo", 50);
+foto2 = new imagen("Cerdo", "Cerdo", 40);
